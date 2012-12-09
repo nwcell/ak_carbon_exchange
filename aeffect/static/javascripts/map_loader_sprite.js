36b26871145d0e8158624020a376207117000570
Shane R. Spencer <sspencer@bigboote.(none)>
2012-12-08 17:29:26 -0900

    template inheritence and lots of fixups.. speedup on mapping

---
+++ aeffect/static/javascripts/map_loader_sprite.js
@@ -0,0 +1,55 @@
+	var cSpeed=9;
+	var cWidth=128;
+	var cHeight=128;
+	var cTotalFrames=10;
+	var cFrameWidth=128;
+	var cImageSrc='/images/ajax-loader-sprites.png';
+	
+	var cImageTimeout=false;
+	var cIndex=0;
+	var cXpos=0;
+	var SECONDS_BETWEEN_FRAMES=0;
+	
+	function startAnimation(){
+		
+		document.getElementById('map_loader').style.backgroundImage='url('+cImageSrc+')';
+		document.getElementById('map_loader').style.width=cWidth+'px';
+		document.getElementById('map_loader').style.height=cHeight+'px';
+		
+		//FPS = Math.round(100/(maxSpeed+2-speed));
+		FPS = Math.round(100/cSpeed);
+		SECONDS_BETWEEN_FRAMES = 1 / FPS;
+		
+		setTimeout('continueAnimation()', SECONDS_BETWEEN_FRAMES/1000);
+		
+	}
+	
+	function continueAnimation(){
+		
+		cXpos += cFrameWidth;
+		//increase the index so we know which frame of our animation we are currently on
+		cIndex += 1;
+		 
+		//if our cIndex is higher than our total number of frames, we're at the end and should restart
+		if (cIndex >= cTotalFrames) {
+			cXpos =0;
+			cIndex=0;
+		}
+		
+		document.getElementById('map_loader').style.backgroundPosition=(-cXpos)+'px 0';
+		
+		setTimeout('continueAnimation()', SECONDS_BETWEEN_FRAMES*1000);
+	}
+	
+	function imageLoader(s, fun)//Pre-loads the sprites image
+	{
+		clearTimeout(cImageTimeout);
+		cImageTimeout=0;
+		genImage = new Image();
+		genImage.onload=function (){cImageTimeout=setTimeout(fun, 0)};
+		genImage.onerror=new Function('alert(\'Could not load the image\')');
+		genImage.src=s;
+	}
+	
+	//The following code starts the animation
+	new imageLoader(cImageSrc, 'startAnimation()');
